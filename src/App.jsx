d2b6e6ff1b1a9e5dce2d2b01c52c179ad12ddf13
elbitor0 <123994826+elbitor0@users.noreply.github.com>
2024-03-13 11:51:40 +0000

    sds

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
        <Routes>
            <Route path="/" element = {<Home/>} />
            <Route path="/List" element = {<ListDrivers/>} />
-           <Route paht ="/table" element = {<TableDrivers/>} />
+           <Route path ="/table" element = {<TableDrivers/>} />
            <Route path="*" element = { <PageNotFound /> } />            
        </Routes>
    </Router>
